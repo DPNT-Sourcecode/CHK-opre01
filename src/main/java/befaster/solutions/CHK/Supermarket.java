931acc2f0204d1af50dd48580a074e39ff6b259f
sbx_user1051 <sbx_user1051@169.254.8.73>
2024-02-05 15:35:42 +0000

    Mon Feb 05 15:35:42 UTC 2024

--- src/main/java/befaster/solutions/CHK/Supermarket.java
+++ src/main/java/befaster/solutions/CHK/Supermarket.java
@@ -5,6 +5,18 @@ import java.util.Map;
 
 public class Supermarket {
 
-    private static final Map<Character, Integer> prices = new HashMap<>();
+    private static final Map<StockKeepingUnits, Integer> prices = new HashMap<>();
     private static final Map<Character, SpecialOffer> specialOffers = new HashMap<>();
+
+    static {
+        prices.put(StockKeepingUnits.A, 50);
+        prices.put(StockKeepingUnits.B, 30);
+        prices.put(StockKeepingUnits.C, 20);
+        prices.put(StockKeepingUnits.D, 15);
+
+        specialOffers.put(StockKeepingUnits.A, new SpecialOffer(3, 130));
+        specialOffers.put(StockKeepingUnits.B, new SpecialOffer(2, 45));
+    }
+
 }
+
