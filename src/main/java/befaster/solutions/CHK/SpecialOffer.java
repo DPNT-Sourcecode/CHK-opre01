d7ddaf6e57663168e970d8df58cb32d726b14cc2
sbx_user1051 <sbx_user1051@169.254.8.73>
2024-02-05 18:36:39 +0000

    Mon Feb 05 18:36:39 UTC 2024

--- src/main/java/befaster/solutions/CHK/SpecialOffer.java
+++ src/main/java/befaster/solutions/CHK/SpecialOffer.java
@@ -1,6 +1,6 @@
 package befaster.solutions.CHK;
 
-public record SpecialOffer(int numberOfItems, int price) implements Offerable{
+public record SpecialOffer(int numberOfItems, int price) implements Offerable, Comparable<SpecialOffer>{
     @Override
     public int getNumberOfItems() {
         return this.numberOfItems;
@@ -9,4 +9,10 @@ public record SpecialOffer(int numberOfItems, int price) implements Offerable{
     public int getPrice() {
         return this.price;
     }
+
+    @Override
+    public int compareTo(final SpecialOffer o) {
+        return 0;
+    }
 }
+
