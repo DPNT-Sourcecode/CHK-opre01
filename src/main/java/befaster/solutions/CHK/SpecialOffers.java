6bc796b866c1948845ca7b0f0ca19a20d68675d1
sbx_user1051 <sbx_user1051@169.254.82.101>
2024-02-14 13:40:13 +0000

    Wed Feb 14 13:40:13 UTC 2024

--- src/main/java/befaster/solutions/CHK/SpecialOffers.java
+++ src/main/java/befaster/solutions/CHK/SpecialOffers.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.EnumMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -137,3 +138,4 @@ public final class SpecialOffers {
         return (discountPrice / originalPrice) * 100;
     }
 }
+
