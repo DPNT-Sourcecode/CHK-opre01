47132e50530a77284aa88df5bd45d0bca93900c2
sbx_user1051 <sbx_user1051@169.254.33.133>
2024-02-15 16:19:28 +0000

    Thu Feb 15 16:19:28 UTC 2024

--- src/main/java/befaster/solutions/CHK/SpecialOffers.java
+++ src/main/java/befaster/solutions/CHK/SpecialOffers.java
@@ -131,8 +131,9 @@ public final class SpecialOffers {
         return offerableList.stream()
                 .sorted((s1, s2) -> {
                     int s1MedianPrice = s1.getSkus().stream().mapToInt(PriceTable::getPrice).reduce(0, Integer::sum) / s1.getNumberOfItems();
-                    double s1Discount = calculateDiscountPercentage(PriceTable.getPrice(s1.getOffer().getSkus().get(0)),
+                    double s1Discount = calculateDiscountPercentage(s1MedianPrice,
                             s1.getOffer().getNumberOfItems(), s1.getOffer().getPrice());
+                    int s1MedianPrice = s1.getSkus().stream().mapToInt(PriceTable::getPrice).reduce(0, Integer::sum) / s1.getNumberOfItems();
                     double s2Discount = calculateDiscountPercentage(PriceTable.getPrice(s2.getOffer().getSkus().get(0)),
                             s2.getOffer().getNumberOfItems(), s2.getOffer().getPrice());
                     return Double.compare(s2Discount, s1Discount);
@@ -334,6 +335,7 @@ public final class SpecialOffers {
     }
 
 }
+
 
 
 
